153d35e534fb9584394dc85d99e1bf3b3e0db13d
Avsitidiiskyi <iavsitidiiskyi@gmail.com>
2026-06-07 19:12:36 +0200

    add PWA manifest and icons

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'ytfree-v1';
+const ASSETS = [
+  '/free_yt_premium/',
+  '/free_yt_premium/index.html',
+  '/free_yt_premium/css/base.css',
+  '/free_yt_premium/css/header.css',
+  '/free_yt_premium/css/sidebar.css',
+  '/free_yt_premium/css/feed.css',
+  '/free_yt_premium/css/player.css',
+  '/free_yt_premium/css/modal.css',
+  '/free_yt_premium/js/storage.js',
+  '/free_yt_premium/js/urlparser.js',
+  '/free_yt_premium/js/api.js',
+  '/free_yt_premium/js/player.js',
+  '/free_yt_premium/js/feed.js',
+  '/free_yt_premium/js/search.js',
+  '/free_yt_premium/js/watchlater.js',
+  '/free_yt_premium/js/channel.js',
+  '/free_yt_premium/js/playlist.js',
+  '/free_yt_premium/js/app.js'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        catches.open(CACHE).then(c => c.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        catches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
